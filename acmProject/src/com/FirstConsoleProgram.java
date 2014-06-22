7a814a98c308f7bdf37e7aadec57bb4b6b2499b5
Super Open <supernameopen@163.com>
2014-06-22 22:00:00 +0800

    delete some blank line for FirstConsoleProgram file

--- acmProject/src/com/FirstConsoleProgram.java
+++ acmProject/src/com/FirstConsoleProgram.java
@@ -13,8 +13,7 @@ public class FirstConsoleProgram extends ConsoleProgram
 //		int n2 = readInt("Enter n2= ");
 //		int sum = n1 + n2;
 //		println("The total is "+ sum);
-		
-		
+	
 		String line = readLine();
 		StringTokenizer stringTokenizer = new StringTokenizer(line);
 		for(int i=0;stringTokenizer.hasMoreTokens();i++)
